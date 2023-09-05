21a37465ea46a52c50f11b2c15904e760569c8e5
Nicolas <rodriguez.g.nicolas@accenture.com>
2023-09-05 13:18:51 -0300

    refactoring controller

--- src/main/java/com/workschedule/app/controllers/RequerimientoController.java
+++ src/main/java/com/workschedule/app/controllers/RequerimientoController.java
@@ -18,14 +18,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -230,6 +223,7 @@ public class RequerimientoController {
 	public String crear(Model model) {
 		Requerimiento requerimiento = new Requerimiento();
 		requerimiento.setEstimacion(new ArrayList<>());
+		//model.addAttribute("fases", listaFases);
 		model.addAttribute("requerimiento", requerimiento);
 		return "requerimiento/form";
 	}
